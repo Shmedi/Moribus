0f7c1c8e534967a995c3c09b727416af58b40e08
Sofia <sofia.lau.alves@gmail.com>
2020-08-16 15:08:27 -0400

    new js code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import firebase from "./firebase";
-import axios from 'axios';
+import axios from "axios";
 
 class App extends Component {
   constructor() {
@@ -8,76 +8,80 @@ class App extends Component {
     this.state = {
       makeupItems: [],
       itemSearch: "",
-      filteredMakeupItems: []
-    }
+      filteredMakeupItems: [],
+    };
   }
 
   componentDidMount() {
     console.log("Mounted");
-    
-    axios({
-        url: "http://makeup-api.herokuapp.com/api/v1/products.json",
-        method: 'GET',
-        dataType: 'json',
-        params: {
-          product_tags: "Vegan",
-          product_type: this.state.itemSearch
-        }
-    })
-    .then( (res) => {
-      const makeupArray = res.data;
 
+    axios({
+      url: "http://makeup-api.herokuapp.com/api/v1/products.json",
+      method: "GET",
+      dataType: "json",
+      params: {
+        product_tags: "Vegan",
+        product_type: this.state.itemSearch,
+      },
+    }).then((res) => {
       this.setState({
-        makeupItems: makeupArray
-      })
+        makeupItems: res.data,
+      });
       //(e.g. price, link to purchase, color values, photo, original rating, would repurchase/wouldn’t repurchase rating)
-      console.log(makeupArray);
-    })
+      // console.log(makeupArray);
+    });
   }
 
-  inputSearch = ( event ) => {
+  inputSearch = (event) => {
     event.preventDefault();
     this.setState({
-      itemSearch: event.target.value
-    })
+      itemSearch: event.target.value,
+    });
     console.log(event.target.value);
-  }
+  };
 
-  handleClick = (event, userChoice) => {
+  handleClick = (event) => {
     event.preventDefault();
+
     const filteredData = this.state.makeupItems.filter((items) => {
-      return items[userChoice] === this.inputSearch(event);
-    })
-    console.log(filteredData);
-    
-  }
+      return items.product_type == this.state.itemSearch;
+    });
+    this.setState({
+      filteredMakeupItems: filteredData,
+    });
+  };
 
   render() {
-    console.log('Rendering...')
+    console.log("Rendering...");
     return (
       <div className="App">
         <h1>Moribus</h1>
         <form action="">
-          <label htmlFor="item">Enter in a product name</label><br/>
-          <input onChange={this.inputSearch} type="textarea" id="item" value={this.state.itemSearch}/><br/>
+          <label htmlFor="item">Enter in a product name</label>
+          <br />
+          <input
+            onChange={this.inputSearch}
+            type="textarea"
+            id="item"
+            value={this.state.itemSearch}
+          />
+          <br />
 
           <button onClick={this.handleClick}>Search</button>
         </form>
-        {/* {
-          this.state.makeupItems.map( (product) => {
-            return (
-              <div>
-                <h2>{product.name}</h2>
-                <p>{product.price}</p>
-                <img src={product.image_link} alt=""/>
-                <p>{product.product_link}</p>
-                <p>{product.description}</p>
-                <p>{product.product_type}</p>
-                <p>{product.tag_list[0]}</p>
-              </div>
-            )
-          })
-        } */}
+        {this.state.filteredMakeupItems.map((product) => {
+          return (
+            <div>
+              <h2>{product.name}</h2>
+              <p>{product.price}</p>
+              <img src={product.image_link} alt="" />
+              <p>{product.product_link}</p>
+              <p>{product.description}</p>
+              <p>{product.product_type}</p>
+              <p>{product.tag_list[0]}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
