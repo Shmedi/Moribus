5a30851694bddc7ba1ff9d94188e593e7dcb8679
Sofia <sofia.lau.alves@gmail.com>
2020-08-18 16:04:16 -0400

    added back button

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import Catalogue from './Catalogue';
 import MakeupDetails from './MakeupDetails';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+
+
+
 class App extends Component {
   render() {
     console.log("Rendering...");
     return (
       <Router>
         <div className="App">
+          
           <header>
             <h1>Moribus</h1>
           </header>
@@ -18,6 +22,13 @@ class App extends Component {
           <Route exact path="/" component={ Catalogue } />
           <Route exact path="/MakeupDetails/:makeupId" component={ MakeupDetails } /> 
 
+
+            <footer> 
+              <div>
+                <p>Copyright &copy; 2020 Moribus</p>
+                <p>Created @ <a href="https://junocollege.com/" target="_blank">Juno College</a></p> 
+              </div>
+          </footer>
         </div>
       </Router> 
     );
