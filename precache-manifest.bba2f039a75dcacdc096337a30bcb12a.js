c8c411962d2c8945e1504e62c73bee5208ab8a0a
Shmedi <eduard.tupy@hotmail.com>
2020-08-26 19:28:11 -0400

    Updates

---
+++ precache-manifest.bba2f039a75dcacdc096337a30bcb12a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8f46c9a444d96f5b3d8deba5a36826",
+    "url": "/Moribus/index.html"
+  },
+  {
+    "revision": "f0983b4b5ed09eb7bbb1",
+    "url": "/Moribus/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "ba5b0bfd056f093c2914",
+    "url": "/Moribus/static/css/main.c4c7f2b3.chunk.css"
+  },
+  {
+    "revision": "f0983b4b5ed09eb7bbb1",
+    "url": "/Moribus/static/js/2.62870e99.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/Moribus/static/js/2.62870e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba5b0bfd056f093c2914",
+    "url": "/Moribus/static/js/main.0e9ee5c8.chunk.js"
+  },
+  {
+    "revision": "af46d216e1ad73cb3b53",
+    "url": "/Moribus/static/js/runtime-main.21ad8030.js"
+  },
+  {
+    "revision": "2ad9bdac61f36ef24439162e10854054",
+    "url": "/Moribus/static/media/bannerImg1.2ad9bdac.jpg"
+  },
+  {
+    "revision": "28d4536d8ab97ea833490654cedefaaa",
+    "url": "/Moribus/static/media/bannerImg2.28d4536d.jpg"
+  },
+  {
+    "revision": "a092f6020742358bbfa46273000ea074",
+    "url": "/Moribus/static/media/bannerImg3.a092f602.jpg"
+  },
+  {
+    "revision": "39202c55768f5e481d936bd3949f71d0",
+    "url": "/Moribus/static/media/bannerImg4.39202c55.jpg"
+  },
+  {
+    "revision": "692f3ab4f35fcfb71026761141a93a98",
+    "url": "/Moribus/static/media/bannerImg5.692f3ab4.jpg"
+  }
+]);
